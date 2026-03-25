2abc038d2c7e18f6c22d6721fa29adecc677e784
NuttyTeeraphong <99229778+teeraphong123@users.noreply.github.com>
2026-03-25 13:40:12 +0700

    Day 3: Error if not input Username and Password

---
+++ script.js
@@ -0,0 +1,26 @@
+document.querySelector("button").addEventListener("click", function (event) {
+    event.preventDefault();
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+
+    let usernameError = document.getElementById("usernameError");
+    let passwordError = document.getElementById("passwordError");
+
+    // reset ก่อน
+    usernameError.textContent = "";
+    passwordError.textContent = "";
+
+    if(username === ""){
+        usernameError.textContent = "กรุณากรอก Username";
+        document.getElementById("username").style.borderColor = 'red';
+    }else if(password === ""){
+        passwordError.textContent = "กรุณากรอก Password";
+        document.getElementById("password").style.borderColor = 'red';
+    }else if(password.length < 6){
+        passwordError.textContent = "กรุณากรอก Password อย่างน้อย 6 ตัว";
+        document.getElementById("password").style.borderColor = 'red';
+    }else{
+        alert("เข้าสู่ระบบเสร็จสิ้น");
+    }
+
+});
